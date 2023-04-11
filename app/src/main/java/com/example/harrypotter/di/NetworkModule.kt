2736b36dc3a1e7ba06ce61b6be2f2c9076b5e2fd
Prabhat Kumar <pkrlal883@gmail.com>
2023-04-11 18:28:43 +0530

    Project Structure and Initial setup Added required libraries

---
+++ app/src/main/java/com/example/harrypotter/di/NetworkModule.kt
@@ -0,0 +1,6 @@
+package com.example.harrypotter.di
+
+
+object NetworkModule {
+
+}
---
+++ app/src/main/java/com/example/harrypotter/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.harrypotter.utils
+
+
+//"https://hp-api.onrender.com/api/characters"
+object Constants {
+
+    const val BASE_URL=""
+}
